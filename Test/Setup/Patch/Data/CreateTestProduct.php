62a31f9e68b774d93f95ce8dc9ec5566cc1449bb
Maciej Dabrowski <maciej.dabrowski@scandiweb.com>
2022-11-18 12:29:20 +0100

    I made the required corrections

--- Test/Setup/Patch/Data/CreateTestProduct.php
+++ Test/Setup/Patch/Data/CreateTestProduct.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Scandiweb\Test\Setup\Patch\Data;
 
-
 use Magento\Catalog\Api\CategoryLinkManagementInterface;
 use Magento\Catalog\Api\Data\ProductInterfaceFactory;
 use Magento\Catalog\Api\ProductRepositoryInterface;
@@ -21,19 +20,75 @@ use Magento\InventoryApi\Api\Data\SourceItemInterface;
 use Magento\InventoryApi\Api\Data\SourceItemInterfaceFactory;
 use Magento\InventoryApi\Api\SourceItemsSaveInterface;
 
+/**
+ * Create Migration product class
+ */
 class CreateTestProduct implements DataPatchInterface
 {
+    /**
+     * @var ModuleDataSetupInterface
+     */
     protected ModuleDataSetupInterface $setup;
+
+    /**
+     * @var ProductInterfaceFactory
+     */
     protected ProductInterfaceFactory $productInterfaceFactory;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
     protected ProductRepositoryInterface $productRepository;
+
+    /**
+     * @var State
+     */
     protected State $appState;
-	protected EavSetup $eavSetup;
+
+    /**
+     * @var EavSetup
+     */
+    protected EavSetup $eavSetup;
+
+    /**
+     * @var StoreManagerInterface
+     */
     protected StoreManagerInterface $storeManager;
-	protected SourceItemInterfaceFactory $sourceItemFactory;
-	protected SourceItemsSaveInterface $sourceItemsSaveInterface;
-	protected CategoryLinkManagementInterface $categoryLink;
+
+    /**
+     * @var SourceItemInterfaceFactory
+     */
+    protected SourceItemInterfaceFactory $sourceItemFactory;
+
+    /**
+     * @var SourceItemsSaveInterface
+     */
+    protected SourceItemsSaveInterface $sourceItemsSaveInterface;
+
+    /**
+     * @var CategoryLinkManagementInterface
+     */
+    protected CategoryLinkManagementInterface $categoryLink;
+
+    /**
+     * @var array
+     */
     protected array $sourceItems = [];
 
+    /**
+     *
+     * Migration patch constructor.
+     *
+     * @param ModuleDataSetupInterface $setup
+     * @param ProductInterfaceFactory $productInterfaceFactory
+     * @param ProductRepositoryInterface $productRepository
+     * @param State $appState
+     * @param StoreManagerInterface $storeManager
+     * @param EavSetup $eavSetup
+     * @param SourceItemInterfaceFactory $sourceItemFactory
+     * @param SourceItemsSaveInterface $sourceItemsSaveInterface
+     * @param CategoryLinkManagementInterface $categoryLink
+     */
     public function __construct(
         ModuleDataSetupInterface $setup,
         ProductInterfaceFactory $productInterfaceFactory,
@@ -41,50 +96,105 @@ class CreateTestProduct implements DataPatchInterface
         State $appState,
         StoreManagerInterface $storeManager,
         EavSetup $eavSetup,
-		SourceItemInterfaceFactory $sourceItemFactory,
+        SourceItemInterfaceFactory $sourceItemFactory,
         SourceItemsSaveInterface $sourceItemsSaveInterface,
-		CategoryLinkManagementInterface $categoryLink
+        CategoryLinkManagementInterface $categoryLink
     ) {
         $this->appState = $appState;
         $this->productInterfaceFactory = $productInterfaceFactory;
         $this->productRepository = $productRepository;
         $this->setup = $setup;
-		$this->eavSetup = $eavSetup;
+        $this->eavSetup = $eavSetup;
         $this->storeManager = $storeManager;
         $this->sourceItemFactory = $sourceItemFactory;
         $this->sourceItemsSaveInterface = $sourceItemsSaveInterface;
-		$this->categoryLink = $categoryLink;
+        $this->categoryLink = $categoryLink;
     }
 
-    public static function getDependencies(): array { return []; }
-    public function getAliases(): array { return []; }
+    /**
+     * {@inheritDoc}
+     * @return array
+     */
+    public static function getDependencies(): array
+    {
+        return [];
+    }
 
-	public function apply(): void
+    /**
+     * {@inheritDoc}
+     * @return array
+     */
+    public function getAliases(): array
+    {
+        return [];
+    }
+
+    /**
+     * Add new product
+     * @return void
+     * @throws \Exception
+     */
+    public function apply(): void
     {
         $this->appState->emulateAreaCode('adminhtml', [$this, 'execute']);
     }
 
-	public function execute(): void
-	{
+    /**
+     * @return void
+     * @throws \Magento\Framework\Exception\CouldNotSaveException
+     * @throws \Magento\Framework\Exception\InputException
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Magento\Framework\Exception\StateException
+     */
+    public function execute(): void
+    {
+        // create the product
         $product = $this->productInterfaceFactory->create();
-
+        // check if the product already exists
         if ($product->getIdBySku('test-product')) {
             return;
         }
-
+        // get the attribute set id from EavSetup object
         $attributeSetId = $this->eavSetup->getAttributeSetId(Product::ENTITY, 'Default');
-        
-		$product->setTypeId(Type::TYPE_SIMPLE)
-        ->setAttributeSetId($attributeSetId)
-        ->setName('Test Product')
-        ->setSku('test-product')
-		->setUrlKey('testproduct')
-        ->setPrice(9.99)
-        ->setVisibility(Visibility::VISIBILITY_BOTH)
-        ->setStatus(Status::STATUS_ENABLED);
+        /**
+         * set product Type
+         * set product Name
+         * set product Sku
+         * set product UrlKey
+         * set product Price
+         * set product Visibility
+         * set product Status
+         * set product Data
+         * save product
+         */
+        $product->setTypeId(Type::TYPE_SIMPLE)
+            ->setAttributeSetId($attributeSetId)
+            ->setName('Test Product')
+            ->setSku('test-product')
+            ->setUrlKey('testproduct')
+            ->setPrice(9.99)
+            ->setVisibility(Visibility::VISIBILITY_BOTH)
+            ->setStatus(Status::STATUS_ENABLED)
+            ->setStockData(['use_config_manage_stock' => 1, 'is_qty_decimal' => 0, 'is_in_stock' => 1]);
         $product = $this->productRepository->save($product);
 
+        /**
+         * Create source item
+         * Set quantity for the source item
+         * Link Source item to product
+         * Set status for the product
+         * Add the source item to the Source items array
+         * Save the source items
+         * Assign category for the product.
+         */
+        $sourceItem = $this->sourceItemFactory->create();
+        $sourceItem->setSourceCode('default');
+        $sourceItem->setQuantity(1000);
+        $sourceItem->setSku($product->getSku('test-product'));
+        $sourceItem->setStatus(SourceItemInterface::STATUS_IN_STOCK);
+        $this->sourceItems[] = $sourceItem;
+        $this->sourceItemsSaveInterface->execute($this->sourceItems);
         $this->categoryLink->assignProductToCategories($product->getSku(), [2]);
-
-	}
+    }
 }
+
